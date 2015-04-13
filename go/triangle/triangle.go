917c6085d8d1fe992ebb6da57173440db5714cb6
Vincent Demeester <vincent@sbr.pm>
2015-04-13 13:37:36 +0200

    Update triangle according to review

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,53 +1,53 @@
 package triangle
 
 import (
-        "math"
+	"math"
 )
 
 type Kind string
 
 const (
-        Equ Kind = "equilateral"
-        Iso Kind = "isoceles"
-        Sca Kind = "scalene"
-        NaT = "not a triangle"
+	Equ Kind = "equilateral"
+	Iso Kind = "isoceles"
+	Sca Kind = "scalene"
+	NaT      = "not a triangle"
 )
 
 type Triangle struct {
-        a, b, c float64
+	a, b, c float64
 }
 
 func KindFromSides(a, b, c float64) Kind {
-        var triangle = Triangle{a: a, b: b, c: c}
-        if isValid(triangle) {
-                if isEquilateral(triangle) {
-                        return Equ
-                }
-                if isIsocele(triangle) {
-                        return Iso
-                }
-                return Sca
-        }
-        return NaT
+	var triangle = Triangle{a: a, b: b, c: c}
+	switch {
+	case !isValid(triangle):
+		return NaT
+	case isEquilateral(triangle):
+		return Equ
+	case isIsocele(triangle):
+		return Iso
+	default:
+		return Sca
+	}
 }
 
 func isValid(triangle Triangle) bool {
-        if !math.IsNaN(triangle.a) && !math.IsNaN(triangle.b) && !math.IsNaN(triangle.c) && triangle.a > 0 && triangle.b > 0 && triangle.c > 0 {
-                if triangleInequality(triangle) {
-                        return true
-                }
-        }
-        return false
+	if !math.IsNaN(triangle.a) && !math.IsNaN(triangle.b) && !math.IsNaN(triangle.c) && triangle.a > 0 && triangle.b > 0 && triangle.c > 0 {
+		if triangleInequality(triangle) {
+			return true
+		}
+	}
+	return false
 }
 
 func triangleInequality(triangle Triangle) bool {
-        return triangle.a + triangle.b > triangle.c && triangle.a + triangle.c > triangle.b && triangle.b + triangle.c > triangle.a
+	return triangle.a+triangle.b > triangle.c && triangle.a+triangle.c > triangle.b && triangle.b+triangle.c > triangle.a
 }
 
 func isIsocele(triangle Triangle) bool {
-        return triangle.a == triangle.b || triangle.b == triangle.c || triangle.a == triangle.c
+	return triangle.a == triangle.b || triangle.b == triangle.c || triangle.a == triangle.c
 }
 
 func isEquilateral(triangle Triangle) bool {
-        return triangle.a == triangle.b && triangle.b == triangle.c
+	return triangle.a == triangle.b && triangle.b == triangle.c
 }
