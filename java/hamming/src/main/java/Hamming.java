b9ba6e3f8c3e2fcf10324a08a95a581ebd6398d9
Vincent Demeester <vincent@sbr.pm>
2015-05-10 22:12:00 +0200

    java: Add hamming implementation

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,2 +1,14 @@
 public class Hamming {
+    public static Integer compute(String sequence1, String sequence2) {
+        if (sequence1.length() != sequence2.length()) {
+            throw new IllegalArgumentException("The 2 DNA sequence should have the same length");
+        }
+        int hammingDistance = 0;
+        for (int index = 0; index < sequence1.length(); index++) {
+            if (sequence1.charAt(index) != sequence2.charAt(index)) {
+                hammingDistance++;
+            }
+        }
+        return hammingDistance;
+    }
 }
