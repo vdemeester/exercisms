4266ed40ba2667fd5e2a6e8a36eb7ce844e19698
Vincent Demeester <vincent@sbr.pm>
2017-07-02 16:00:11 +0200

    Add elisp hello world
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- elisp/hello-world/hello-world.el
+++ elisp/hello-world/hello-world.el
@@ -3,7 +3,11 @@
 ;;; Commentary:
 
 ;;; Code:
-
+(defun hello (&optional name)
+  "Hello world, first exercism"
+  (or name (setq name "World"))
+  (format "Hello, %s!" name)
+  )
 
 (provide 'hello-world)
 ;;; hello-world.el ends here
