167f7c87dc4bf6d1d10ef2405257eb70fd2b42de
Vincent Demeester <vincent@sbr.pm>
2017-08-15 17:49:19 +0200

    elisp: difference of squares iteration 1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- elisp/difference-of-squares/difference-of-squares.el
+++ elisp/difference-of-squares/difference-of-squares.el
@@ -1,9 +1,25 @@
 ;;; difference-of-squares.el --- Difference of Squares (exercism)
+;; -*- firestarter: ert-run-tests-interactively -*-
 
 ;;; Commentary:
 
 ;;; Code:
+(defun square-of-sums (number)
+  "Compute the square of sums"
+  (square (apply '+ (number-sequence 1 number))))
 
+(defun sum-of-squares (number)
+  "Compute the sum of squares"
+  (apply '+ (seq-map 'square (number-sequence 1 number)))
+  )
+
+(defun difference (number)
+  "Compute the difference of sums"
+  (- (square-of-sums number) (sum-of-squares number)))
+
+(defun square (number)
+  "Compute the square of a number"
+  (* number number))
 
 (provide 'difference-of-squares)
 ;;; difference-of-squares.el ends here
