cf41345c3bb5e28ce50ca7ea230e021a7bb3727c
Vincent Demeester <vincent@sbr.pm>
2018-09-19 17:34:43 +0200

    Update default.nix (simplifying it)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- default.nix
+++ default.nix
@@ -1,30 +1,18 @@
-let
-	_pkgs = import <nixpkgs> {};
-in
-	{ pkgs ? import (_pkgs.fetchFromGitHub {
-		owner = "NixOS";
-		repo = "nixpkgs-channels";
-		rev = "d0d905668c010b65795b57afdf7f0360aac6245b";
-		sha256 = "1kqxfmsik1s1jsmim20n5l4kq6wq8743h5h17igfxxbbwwqry88l";
-	}) {}
-}:
-
-pkgs.stdenv.mkDerivation rec {
-	name = "exercisms";
-	env = pkgs.buildEnv { name = name; paths = buildInputs; };
-	buildInputs = [
-		pkgs.exercism
-		pkgs.go_1_9
-		pkgs.openjdk
-		pkgs.clojure
-		pkgs.leiningen
-		pkgs.python
-		pkgs.ghc
-		pkgs.stack
-		pkgs.hlint
-		pkgs.emacs
-		pkgs.rustc
-		pkgs.sbcl
-		pkgs.lispPackages.quicklisp
-	];
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "exercisms";
+  buildInputs = with pkgs; [
+  	exercism
+		openjdk
+		clojure
+		leiningen
+    go
+    python
+		ghc
+		stack
+		hlint
+		rustup
+		sbcl
+		lispPackages.quicklisp
+  ];
 }
